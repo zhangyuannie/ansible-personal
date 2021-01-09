616525bc1b135c343b1948978419e082ca8cd555
Zhangyuan Nie <yuan@znie.org>
2021-01-08 21:44:53 -0500

    user: add code to nautilus' background context menu

---
+++ roles/workstation-user/files/home/.local/share/nautilus-python/extensions/open-code.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+#
+# Add context menu to open Visual Studio Code to Nautilus
+#
+# Copyright (c) Zhangyuan Nie. All rights reserved. MIT license.
+
+import gi
+
+gi.require_version("Gtk", "3.0")
+gi.require_version("Nautilus", "3.0")
+from gi.repository import Nautilus, GObject
+from subprocess import run
+
+
+class OpenCodeExtension(GObject.GObject, Nautilus.MenuProvider):
+    def _open_code(self, menu, file):
+        run(["code", file.get_location().get_path()])
+
+    def get_background_items(self, window, file):
+        item = Nautilus.MenuItem(
+            name="NautilusPython::opencode",
+            label="Open in Code",
+            tip="Open the current directory in Code",
+        )
+        item.connect("activate", self._open_code, file)
+
+        return (item,)
